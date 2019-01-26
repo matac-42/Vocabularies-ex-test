6e94e3ee94da7a5cb64df6f2dd5f559adf8e0026
Yuto Matayoshi <e185742@ie.u-ryukyu.ac.jp>
2019-01-26 14:01:28 +0900

    Created the "ListFiles()" method.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args){
 
-        String wordsFileName = "/Users/e185742/prog2/Vocabularies_exercise/doc/wordset2";
 
         VocaEx vocaEx = new VocaEx();
-        vocaEx.splitSet(wordsFileName);
+        vocaEx.ListFiles();
+
+        vocaEx.splitSet();
         vocaEx.quiz();
     }
 }
--- src/main/VocaEx.java
+++ src/main/VocaEx.java
@@ -4,15 +4,26 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Vocabularies exercises for final exam.
+ */
+
 public class VocaEx {
 
+    String wordsFileName = "/Users/e185742/prog2/Vocabularies_exercise/wordfile/set";
     private List<String[]> list = new ArrayList<>();
     private String[][] wordSet;
 
+    public void selectWordSet(){
 
-    public void splitSet(String words){
+    }
+
+    /**
+     * Formatting wordSet for extend file.
+     */
+    public void splitSet(){
         try{
-            Scanner scanner = new Scanner(new File(words));
+            Scanner scanner = new Scanner(new File(wordsFileName));
             scanner.useDelimiter("\n");
 //            int counter = 0;
             while(scanner.hasNext()){
@@ -28,6 +39,22 @@ public class VocaEx {
 
     }
 
+    /**
+     * Output file name list, in the wordfile.
+     */
+    public void ListFiles(){
+        File dir = new File("/Users/e185742/prog2/Vocabularies_exercise/wordfile/");
+        String[] list = dir.list();
+        System.out.println("[ファイル一覧]-------------------");
+        for(String fileName : list){
+            System.out.println(fileName.replace("/Users/e185742/prog2/Vocabularies_exercise/wordfile/",""));
+        }
+        System.out.println("-------------------------------");
+    }
+
+    /**
+     * Represent the movement of the quiz.
+     */
     public void quiz(){
         for(int i = 0; i< wordSet.length; i++){
             System.out.println(wordSet[i][0]+ " 何番？");
